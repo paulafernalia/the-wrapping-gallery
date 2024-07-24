e3f991202efed5b0d1f533485dbb318a795b0d94
paulafernalia <paula.fermin.cueto@gmail.com>
2024-07-24 15:47:37 +0100

    Add default value for secret key

--- wrapping/wrapping/settings/base.py
+++ wrapping/wrapping/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='default-development-secret-key')
 
 # Load Supabase configuration from environment variables
 SUPABASE_URL = config('SUPABASE_URL', default='https://default.supabase.co')
