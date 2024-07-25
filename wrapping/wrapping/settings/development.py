248385b24d31afa853e13433cc5a1847eb416a59
paulafernalia <paula.fermin.cueto@gmail.com>
2024-07-25 15:29:24 +0100

    Use SQlite for unittesting

--- wrapping/wrapping/settings/development.py
+++ wrapping/wrapping/settings/development.py
@@ -128,10 +128,6 @@ DATABASES = {
 # Test Database Configuration
 if 'TEST' in os.environ:
     DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': f"test_{config('DB_NAME')}",  # Ensures unique test DB names
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default='5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),  # Unique name for test database
     }
