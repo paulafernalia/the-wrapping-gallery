86120f1772e9b5970460d0183ba5c31170654ef4
paulafernalia <paula.fermin.cueto@gmail.com>
2024-07-25 16:46:28 +0100

    Pass url to front end, modify generate_signed_url to use a different bucket

--- wrapping/wrappinggallery/utils.py
+++ wrapping/wrappinggallery/utils.py
@@ -1,11 +1,14 @@
 from supabase import create_client
 from django.conf import settings
 
-def generate_signed_url(file_path):
+def generate_signed_url(file_path, bucket=None):
+    if bucket is None:
+        bucket = settings.SUPABASE_BUCKET_NAME
+
     supabase = create_client(settings.SUPABASE_URL, settings.SERVICE_ROLE_KEY)
     
     try:
-        response = supabase.storage.from_(settings.SUPABASE_BUCKET_NAME).create_signed_url(file_path, expires_in=3600)
+        response = supabase.storage.from_(bucket).create_signed_url(file_path, expires_in=3600)
         return response['signedURL']
 
     except Exception as e:
