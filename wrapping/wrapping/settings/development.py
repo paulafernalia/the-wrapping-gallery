ad0363da67a58a12c6e7fcfe2cd6090b23acff9e
paulafernalia <paula.fermin.cueto@gmail.com>
2024-08-11 18:10:49 +0100

    Stop printing which settings are being used

--- wrapping/wrapping/settings/development.py
+++ wrapping/wrapping/settings/development.py
@@ -109,7 +109,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-print("DEVELOPMENT SETTINGS")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "/static/"
--- wrapping/wrapping/settings/production.py
+++ wrapping/wrapping/settings/production.py
@@ -108,8 +108,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-print("PRODUCTION SETTINGS")
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "/static/"
