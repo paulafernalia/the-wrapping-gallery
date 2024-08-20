42d0328d3bf5849cffae8f8168460902d37513ac
paulafernalia <paula.fermin.cueto@gmail.com>
2024-08-20 04:06:36 +0100

    Remove unused STATIC_DIRS

--- wrapping/wrapping/settings/production.py
+++ wrapping/wrapping/settings/production.py
@@ -111,7 +111,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_DIRS = [ BASE_DIR / "static", ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
