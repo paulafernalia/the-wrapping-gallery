91d116b1db746c9ed3dfb6e8bfff7721d2cb3ebc
paulafernalia <paula.fermin.cueto@gmail.com>
2024-08-23 11:11:47 +0100

    specify env file to read by decouple

--- wrapping/wrapping/settings/production.py
+++ wrapping/wrapping/settings/production.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config, Csv
+from decouple import Config, RepositoryEnv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+config = Config(RepositoryEnv('.env.prod'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
