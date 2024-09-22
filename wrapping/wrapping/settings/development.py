a687a179c958591d6e14bc8b89192129e395fabb
paulafernalia <paula.fermin.cueto@gmail.com>
2024-09-22 11:10:51 +0100

    Add email settings

--- wrapping/wrapping/settings/development.py
+++ wrapping/wrapping/settings/development.py
@@ -81,6 +81,19 @@ TEMPLATES = [
     },
 ]
 
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default=""),
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default=""),
+
+
+
+
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
--- wrapping/wrapping/settings/production.py
+++ wrapping/wrapping/settings/production.py
@@ -127,6 +127,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default=""),
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default=""),
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
