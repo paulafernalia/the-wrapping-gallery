f39cb212368c34ca4dcbfd9e659c7418139f50cf
paulafernalia <paula.fermin.cueto@gmail.com>
2024-10-08 10:18:05 +0100

    Create separate settings files for development testing and production

--- wrapping/manage.py
+++ wrapping/manage.py
@@ -7,10 +7,16 @@ from decouple import config
 
 def main():
     """Run administrative tasks."""
-    settings_module = config(
-        "DJANGO_SETTINGS_MODULE", default="wrapping.settings.development"
-    )
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+    # Check if the test command is being run
+    if "test" in sys.argv:
+        # Automatically switch to test settings
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wrapping.settings.test")
+    else:
+        # Use the configured or default settings for other commands
+        settings_module = config(
+            "DJANGO_SETTINGS_MODULE", default="wrapping.settings.development"
+        )
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
     try:
         from django.core.management import execute_from_command_line
